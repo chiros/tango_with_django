09728016c417dee5860aa36927327834dd40b91f
顾友勇 <chiros86@163.com>
2016-03-24 15:28:42 +0800

    Django Programe 6.91

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from web.models import Category, Page
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
