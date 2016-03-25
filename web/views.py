48ca6d71e6e706acfd36b3f4753ee2aafe437cff
顾友勇 <chiros86@163.com>
2016-03-25 10:28:14 +0800

    Django Programe 8.3

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Category, Page
+from .forms import CategoryForm, PageForm
 
 # Create your views here.
 def index(request):
@@ -16,8 +17,58 @@ def category(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
         pages = Page.objects.filter(category=category)
+        context_dict['category_name_slug'] = category_name_slug
         context_dict['pages'] = pages
         context_dict['category'] = category
-    except Categoty.DoesNotExist:
-        pass
+    except (KeyError, Category.DoesNotExist):
+        return render(request, 'web/index.html', {})
     return render(request, 'web/category.html', context_dict)
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print(form.errors)
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'web/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'web/add_page.html', context_dict)
