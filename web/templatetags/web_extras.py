7d0f4b7a9aabcbdd09c059430684b275d55e152f
顾友勇 <chiros86@163.com>
2016-03-28 14:16:18 +0800

    Django Programe 14.3

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -4,5 +4,5 @@ from web.models import Category
 register = template.Library()
 
 @register.inclusion_tag('web/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
+def get_category_list(cat=None):
+    return {'cats': Category.objects.all(), 'act_cat': cat}
